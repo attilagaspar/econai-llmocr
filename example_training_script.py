c67a8d83beb34d63301eaf05ba07f7475a79f257
Attila Gáspár <attila.gaspar@gmail.com>
2025-07-18 14:40:47 +0200

    remove clutterű

--- example_training_script.py
+++
@@ -1,9 +0,0 @@
-from efficient_ocr import EffOCR
-
-
-effocr = EffOCR(
-    '../traindata_ocr/config_proba.yaml',
-    '../traindata_ocr/labels.json',
-    '../traindata_ocr/images',
-   )
-effocr.train(target=['char_recognizer'])
--- save_pretrain_trocr.py
+++
@@ -1,4 +0,0 @@
-from transformers import TrOCRProcessor
-
-processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-stage1")
-processor.save_pretrained("trocr_finetuned")
